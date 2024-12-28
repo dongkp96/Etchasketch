831acc73be6d73724aa307c60cccefb697f67181
kd pc <dongkp96@gmail.com>
2024-12-28 10:49:42 -0500

    Added CSS to center and style grid

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-let container = document.querySelector("#container");
+const container = document.querySelector("#container");
 
+let i = 0;
+while (i<256){
+    const div = document.createElement("div");
+    div.setAttribute("class", "panel");
+    div.innerHTML ="&nbsp"
+    
+    container.appendChild(div);
+    i++;
+}
